c207469504f37bbad4a71e6175ad462c0dcd2cd1
Mika Rannisto <rannisto@student.chalmers.se>
2023-04-20 11:04:30 +0200

    #4
    Draft of formatting of monolithic code beginning

--- displayDistance/displayDistance.ino
+++ displayDistance/displayDistance.ino
@@ -1,18 +1,23 @@
 #include "Ultrasonic.h"
 #include "TFT_eSPI.h"
-TFT_eSPI tft;
 
+//Class Initializers
+TFT_eSPI tft;
 Ultrasonic ultrasonic(0);
+
 void setup()
 {
  Serial.begin(9600);
 }
 void loop()
 {
+
+ //diplayDistance ==============================================================================================================================
  long RangeInCentimeters;
-//Measues distance in CM with short delay to ensure fast and accurate measurements
+ //Measues distance in CM with short delay to ensure fast and accurate measurements
  RangeInCentimeters = ultrasonic.MeasureInCentimeters();
  Serial.print(RangeInCentimeters);//0~400cm
  Serial.println(" cm");
+ //Short delay to ensure quick refreshes of data measurements
  delay(100);
 }
