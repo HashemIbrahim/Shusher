6225329f7da3c8c900fb04020d4e948fb8354ee2
johan <gusengjoau@student.gu.se>
2023-04-25 13:03:54 +0200

    Base functionality for the RGB LED Stick, still needs to be checked with wio terminal and connected with the loudness sensor to see if the code works as intended.

--- ShusherCode/ShusherCode.ino
+++ ShusherCode/ShusherCode.ino
@@ -1,6 +1,10 @@
 int val;
 int loudness;
 int Thresholds[] = {20,40,60,80,100,120,140,160,180,200};
+// 
+#include <Adafruit_NeoPixel.h>
+#define NUM_LEDS 10
+#define DATA_PIN 0
 
 String message;
 
@@ -67,6 +71,76 @@ return message;
   }
 return loudness;  
   }
+// RGB LED Stick Functions
+void ledStartupTest(){
+  strip.begin();
+  strip.clear();
+  for(int i= 0; i<NUM_LEDS;i++){
+    delay(200);
+    if(i<3){
+    strip.setPixelColor(i, 255,0,0);
+  }
+    else if(i<7){
+    strip.setPixelColor(i,255,255,0);
+  }
+
+    else if(i<10) {
+    strip.setPixelColor(i,0,255,0);
+  }
+    else if(i==(NUM_LEDS -1)){
+      for(int i = 0; < 3; i++){
+        delay(200);
+        for(int j = 0; j<NUM_LEDS; i++ ){
+        strip.setPixelColor(i,0,255,0) 
+        strip.show();     
+      }
+      delay(300);
+      strip.clear();
+      strip.show();
+    }
+  }
+  
+  
+  
+  strip.show();  
+}
+for (int i = 0; i<NUM_LEDS; i++) {
+  delay(300);
+  strip.setPixelColor(i, 0,0,0);
+}
+}
+void setLedStick(){
+  
+  if(loudness >= 1)
+    strip.setPixelColor(0, 255,0,0);
+  if(loudness >=2)
+    strip.setPixelColor(1,255,0,0);
+  if(loudness >=3)
+    strip.setPixelColor(2,255,0,0);
+  if (loudness >=4)
+    strip.setPixelColor(3,255,255,0);
+  if (loudness >=5)
+    strip.setPixelColor(4,255,255,0); 
+  if (loudness >=6)
+    strip.setPixelColor(5,255,255,0);
+  if (loudness >=7)
+    strip.setPixelColor(6,255,255,0);
+  if (loudness >=8)
+    strip.setPixelColor(7,0,255,0);
+  if (loudness >=9)
+    strip.setPixelColor(8,0,255,0);
+   
+   
+    while(loudness >= 10){
+      strip.clear();
+      delay(150);
+      for(int i = 0; i<NUM_LEDS; i++){
+        strip.setPixelColor(i,0,255,0);       
+      }
+  }
+  } 
+
+
 //----------------------------------------------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------
 
