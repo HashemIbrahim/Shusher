f04defe9de0218b25035128d33141703eaecb212
johan <gusengjoau@student.gu.se>
2023-04-26 12:07:18 +0200

    added some small fixes to the code after testing it with the terminal, code is now ready to merge with the loudness sensor code

--- ShusherCode/ShusherCode.ino
+++ ShusherCode/ShusherCode.ino
@@ -1,16 +1,21 @@
 int val;
 int loudness;
 int Thresholds[] = {20,40,60,80,100,120,140,160,180,200};
-// 
+
+
+// RGB LED Stick-------------------------------------------------------
 #include <Adafruit_NeoPixel.h>
 #define NUM_LEDS 10
 #define DATA_PIN 0
+Adafruit_NeoPixel strip(NUM_LEDS, DATA_PIN, NEO_RGB);
+//----------------------------------------------------------------------
 
 String message;
 
 void setup() {
   // put your setup code here, to run once:
  Serial.begin(9600);
+ ledStartupTest();
 }
 
 void loop() {
@@ -18,6 +23,7 @@ void loop() {
 LoudnessSensorLoudValue();
 Messagecalculator();
 Serial.println(message);
+setLedStick();
 
 	delay(200);
 
@@ -72,72 +78,88 @@ return message;
 return loudness;  
   }
 // RGB LED Stick Functions
-void ledStartupTest(){
+void ledStartupTest(){    // Testing that all LEDs work(LightShow ;) )
   strip.begin();
-  strip.clear();
-  for(int i= 0; i<NUM_LEDS;i++){
+  strip.setBrightness(150);
+  strip.clear(); 
+  for(int i= 0; i<=NUM_LEDS;i++){
     delay(200);
     if(i<3){
-    strip.setPixelColor(i, 255,0,0);
+    strip.setPixelColor(i, 255,0,0);       //setting the color of led number 1-3 to green
+    
   }
     else if(i<7){
-    strip.setPixelColor(i,255,255,0);
+    strip.setPixelColor(i,255,255,0);    //setting the color of led number 4-7 yellow
   }
 
     else if(i<10) {
-    strip.setPixelColor(i,0,255,0);
+    strip.setPixelColor(i,0,255,0);       //Setting the color of LED number 8-9 red
   }
-    else if(i==(NUM_LEDS -1)){
-      for(int i = 0; < 3; i++){
+    else if(i== 10){                      //the whole Stick flashes red when the last LED is reached.
+       for(int j = 0; j < 5; j++){
+        for(int k = 0; k < NUM_LEDS; k++){
+          strip.setPixelColor(k, 0, 255, 0);
+        }
+        strip.show();
         delay(200);
-        for(int j = 0; j<NUM_LEDS; i++ ){
-        strip.setPixelColor(i,0,255,0) 
-        strip.show();     
-      }
-      delay(300);
-      strip.clear();
-      strip.show();
+        strip.clear();
+        strip.show();
+        delay(200);
+        }
     }
+    delay(150);
+    strip.show();
   }
-  
-  
-  
-  strip.show();  
+  strip.clear();
+
 }
-for (int i = 0; i<NUM_LEDS; i++) {
-  delay(300);
-  strip.setPixelColor(i, 0,0,0);
-}
-}
-void setLedStick(){
+
+
+void setLedStick(){                 //Activating the LEDs dependent on the loudness which is determined by the thresholds set at the top.
   
-  if(loudness >= 1)
+  if(loudness >= 1){
     strip.setPixelColor(0, 255,0,0);
-  if(loudness >=2)
-    strip.setPixelColor(1,255,0,0);
-  if(loudness >=3)
-    strip.setPixelColor(2,255,0,0);
-  if (loudness >=4)
-    strip.setPixelColor(3,255,255,0);
-  if (loudness >=5)
-    strip.setPixelColor(4,255,255,0); 
-  if (loudness >=6)
-    strip.setPixelColor(5,255,255,0);
-  if (loudness >=7)
-    strip.setPixelColor(6,255,255,0);
-  if (loudness >=8)
-    strip.setPixelColor(7,0,255,0);
-  if (loudness >=9)
-    strip.setPixelColor(8,0,255,0);
-   
-   
-    while(loudness >= 10){
-      strip.clear();
-      delay(150);
-      for(int i = 0; i<NUM_LEDS; i++){
-        strip.setPixelColor(i,0,255,0);       
-      }
   }
+  if(loudness >= 2){
+    strip.setPixelColor(1, 255,0,0);
+  }
+  if(loudness >= 3){
+    strip.setPixelColor(2, 255,0,0);
+  }
+  if(loudness >= 4){
+    strip.setPixelColor(3, 255,255,0);
+  }
+  if(loudness >= 5){
+    strip.setPixelColor(4, 255,255,0);
+  }
+  if(loudness >= 6){
+    strip.setPixelColor(5, 255,255,0);
+  }
+  if(loudness >= 7){
+    strip.setPixelColor(6, 255,255,0);
+  }
+  if(loudness >= 8){
+    strip.setPixelColor(7, 0,255,0);
+  }
+  if(loudness >= 9){
+    strip.setPixelColor(8, 0,255,0);
+  }
+  
+   
+    while(loudness >= 10){                 //when the stick is at maximum(meaning led number 10) the whole stick flashes red
+     for(int j = 0; j < 5; j++){
+        for(int k = 0; k < NUM_LEDS; k++){
+          strip.setPixelColor(k, 0, 255, 0);
+        }
+        strip.show();
+        delay(200);
+        strip.clear();
+        strip.show();
+        delay(200);
+        }
+    }
+  
+  strip.show();
   } 
 
 
